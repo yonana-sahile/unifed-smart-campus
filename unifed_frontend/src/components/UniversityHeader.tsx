f67627b7e98c84dc5480dd2ba15528841f760c63
Yonas Sahile <yonassahile8@gmail.com>
2026-08-30 01:01:28 +0300

    fix(UniversityHeader): use import type for User

--- unifed_frontend/src/components/UniversityHeader.tsx
+++ unifed_frontend/src/components/UniversityHeader.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { User } from "../types";
+import  type { User } from "../types";
 import { useTheme } from "../context/ThemeContext";
 import { Clock, Shield, Award, Calendar, Bell, Globe, CheckCircle2, ChevronDown, BookOpen, Sun, Moon } from "lucide-react";
 import { motion } from "motion/react";
