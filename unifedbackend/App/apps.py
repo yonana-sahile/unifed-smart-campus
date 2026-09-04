ab0e559e0b03ecebd8b8ce392b8d8e74a1dbde91
Yonas Sahile <yonassahile8@gmail.com>
2026-09-05 00:21:18 +0300

    feat: i do the app config of the backend of my project

--- unifedbackend/App/apps.py
+++ unifedbackend/App/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class AppConfig(AppConfig):
-    name = "App"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'App'
+    verbose_name = "Unified Smart Campus Management System"
